69443e420de239202b26f8d63ad7a3e81216faa5
estelito02 <estelito02@gmail.com>
2019-05-27 22:46:44 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Conversor from './compnents/Conversor'
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>CONVERSOR DE MOEDAS </h1>
+
+      <div className="linha">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+
+      <div className="linha">
+      <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+
+      <div className="linha">
+      <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+      </div>
+
+
+    </div>
+    );
+  }
+  
+  export default App;
